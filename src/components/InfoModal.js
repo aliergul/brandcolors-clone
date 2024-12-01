f40ede347528b764aee097ccbb656306b563970f
ali <ali.ergul@pusula.net.tr>
2024-12-01 15:37:15 +0300

    changes at info modal

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -16,13 +16,14 @@ const InfoModal = ({ open, handleModal }) => {
       </button>
       <h3>About BrandColors</h3>
       <p>
-        BrandColors was created by DesignBombs. The goal was to create a helpful
-        reference for the brand color codes that are needed most often.
+        BrandColors was created by <b>DesignBombs</b>. The goal was to create a
+        helpful reference for the brand color codes that are needed most often.
       </p>
       <p>
         It's been featured by Smashing Magazine, CSS-Tricks, Web Design Depot,
-        Tuts+, and over 2 million pageviews. There are now over 600 brands with
-        1600 colors and the collection is always growing.
+        Tuts+, and over <b>2 million pageviews</b>. There are now over{" "}
+        <b>600 brands</b> with <b>1600 colors</b> and the collection is always
+        growing.
       </p>
     </Modal>
   );
